e9ba5e2e848a92cc35be5128722fd88a62e5c892
Volkan Gurbuz <volkan.guerbuez@spreadgroup.com>
2022-12-26 12:46:03 +0100

    Initial commit

---
+++ src/main/kotlin/dev/volkangurbuz/interstellarapp/InterstellarAppApplication.kt
@@ -0,0 +1,11 @@
+package dev.volkangurbuz.interstellarapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterstellarAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<InterstellarAppApplication>(*args)
+}
---
+++ src/test/kotlin/dev/volkangurbuz/interstellarapp/InterstellarAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.volkangurbuz.interstellarapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InterstellarAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
